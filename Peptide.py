90b53a22d68f69e6a3dcd5f286b0035adbf96d31
Eugene Melamud <eugene@calicolabs.com>
2017-07-17 17:36:50 -0700

    First commit: Theoretical Ion Generation and Protein digest

---
+++ Peptide.py
@@ -0,0 +1,93 @@
+from MolecularWeights import *
+
+class Peptide:
+    def __init__(self, sequence,  modifications=dict()):
+        self.sequence = sequence
+        self.mods = modifications       ##dictionary example { 5: "C[160]" }
+
+    def getMZ(self, charge):
+
+        M=0
+        for aa in self.sequence:
+            M += C12MASS[aa]
+
+        if self.mods:
+            for m in self.mods.values():
+                if m in MODS: 
+                    M += MODS[m]
+                else:
+                    print "Unknown modification type:", m 
+
+        if charge >= 1:
+            return((M + mH2O + mProton*charge)/charge)
+        else:
+            return(M + mH2O + mProton)
+
+
+    def all_ions(self, ionseries=['b','y'], frg_z_list=[1,2], fragmentlossgains=[0,], mass_limits=None):
+
+        left  =  0
+        L =  len(self.sequence)
+        M =  self.getMZ(0);
+
+        if "n" in self.mods: 
+            left += MODS[ self.mods["n"] ]     #n-terminal mod
+
+        ALLFRAG=list()
+        for pos in range(0,len(self.sequence)-1):
+            aa = self.sequence[pos]
+
+            #split peptide into two segmentes
+            aanum  = pos+1
+            aamass = C12MASS[aa]
+            if aanum in self.mods: aamass += MODS[ self.mods[aanum] ]
+
+            left = left + aamass            #mass of the peptide to the left of break point
+            right = M - left                #mass of the peptide to the right of break point
+
+            #calculate m/z for different charges, gains and loses, and type of ion
+            for z in frg_z_list:
+                for W in fragmentlossgains:
+                    for ion in ionseries:
+
+                        mz=0
+                        ion_num = aanum
+
+                        if ion == 'b':
+                            mz = (left + z*mProton+W)/z
+                        elif ion == 'y':
+                            ion_num = L - aanum
+                            mz =  (right+(z-1)*mProton+W)/z
+
+                        elif ion == 'a':
+                            mz = (left -mCO + z*mProton+W)/z
+                        elif ion == 'x':
+                            ion_num = L - aanum
+                            mz =  (right +mCO -mH2 + (z-1)*mProton+W)/z
+
+                        elif ion == 'c':
+                            mz = (left +mNH3 + z*mProton+W)/z
+                        elif ion == 'z':
+                            ion_num = L - aanum
+                            mz =  (right -mNH3 + (z-1)*mProton+W)/z
+
+                        if mz > 0 and mz >= mass_limits[0] and mz <= mass_limits[1]:
+                            #print ion, ion_num, z, int(W), mz
+                            ALLFRAG.append((ion, ion_num, z, int(W), mz))
+        return(ALLFRAG)
+
+def test_peptide_class():
+        #p1  = Peptide('CKDALA',modifications={1: "C[160]"})
+        p1  = Peptide('CKDALA',modifications={1: "C[160]", "n": "n[230]", 2: "K[357]"})
+
+        print "preMz: ", p1.getMZ(1)
+        print "preMz: ", p1.getMZ(2)
+        print "preMz: ", p1.getMZ(3)
+        print "preMz: ", p1.getMZ(4)
+
+        all_ions = p1.all_ions( ionseries = ['b','y','a'], 
+                frg_z_list = [1,2],
+                fragmentlossgains = [0, -mNH3, -mH2O ], 
+                mass_limits = [80,3000]
+                )
+
---
+++ digest.py
@@ -0,0 +1,65 @@
+from Protein import *
+from Peptide import *
+
+#load protein sequence from fasta file
+def readFasta(filename): 
+    f = open(filename)
+
+    proteins=list()
+    seq=""
+    seq_id=""
+    seq_desc=""
+
+    for line in f:
+        line = line.rstrip().lstrip()
+        if len(line) == 0: continue
+
+        if line[0] == ">":
+            if len(seq):
+                proteins.append(Protein(seq_id,seq_desc,seq))
+                seq=""
+            seq_id, seq_desc = line.split(" ",1)
+        else:
+            seq += line.replace(" ","").upper()
+
+    #last sequence
+    if len(seq):
+            proteins.append(Protein(seq_id,seq_desc,seq))
+    return(proteins)
+
+
+
+proteins = readFasta("example.fasta")
+
+for protein in proteins:
+
+    #
+    #complete tryptic digest
+    #
+    peptides = protein.digest(pattern="[KR]", minlength=5, maxmiss=3, maxlength=30,missprob=0)
+
+
+    #
+    #partial tryptic digest missing 50% of cuts
+    #
+    #peptides = protein.digest(pattern="[KR]", minlength=10, maxmiss=-1, maxlength=30,missprob=0.5)
+
+    #
+    # generate theoretical spectra
+    #
+    for peptide_sequence in peptides:
+        pep = Peptide(peptide_sequence)
+
+        #digest
+        all_ions = pep.all_ions( ionseries = ['b','y','a'], 
+                frg_z_list = [1,2],
+                fragmentlossgains = [0, -mNH3, -mH2O ], 
+                mass_limits = [80,50000]
+                )
+
+        print protein.id, peptide_sequence, "preMz: ", pep.getMZ(2)
+
+        #print theory digest
+        for ion in all_ions: 
+            print "\t", ion
+
